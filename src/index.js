60581e24544a6244e73057e81ac7e297b496c8a2
bluecrest22 <109240109+bluecrest22@users.noreply.github.com>
2025-02-19 18:54:04 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import odinImage from "./odin.png";
+
+console.log("Hello");
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
